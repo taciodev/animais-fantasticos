3098ad383fbaef0a18a5289f6b347d9e9a738c82
Taciano06 <82884780+Taciano06@users.noreply.github.com>
2021-12-19 20:10:25 -0300

    Subindo projeto

---
+++ js/script.js
@@ -0,0 +1,14 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+/* import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordionNav from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js"; */
+
+
+initScrollSuave();
+/* initAnimacaoScroll();
+initAccordionNav();
+initTabNav(); */
+
+
+
+
